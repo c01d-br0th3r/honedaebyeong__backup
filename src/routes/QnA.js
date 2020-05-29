41539dadef4a105121db8290db686b39793511c3
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-05-29 16:53:39 +0900

    make about page

--- src/routes/QnA.js
+++ src/routes/QnA.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Accordion, Icon } from "semantic-ui-react";
 import styled from "styled-components";
-import PersonalInfo from "../components/PersonalInfo";
-import axios from "axios";
+import { Link } from "react-router-dom";
 import Header from "../components/Header";
+import "./QnA.css";
 
 const Container = styled.div`
   display: flex;
@@ -11,146 +10,135 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   padding: 30px 50px;
-  background: linear-gradient(
+  /*background: linear-gradient(
       to bottom,
       rgba(255, 255, 255, 1) 1%,
       rgba(255, 255, 255, 0.2)
     ),
-    url("https://images.unsplash.com/photo-1494236581341-7d38b2e7d824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1888&q=80");
+    url("https://images.unsplash.com/photo-1494236581341-7d38b2e7d824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1888&q=80");*/
   background-position: center;
   background-size: cover;
 `;
 
-const SHeader = styled.div`
-  font-family: "Song Myung", serif;
-  margin-bottom: 30px;
-  padding: 5px 10px;
-  border-bottom: 3px solid #fdcb6e;
-  align-self: flex-start;
-  font-size: 32px;
-`;
-
-const SAccordion = styled(Accordion)`
-  font-family: "Song Myung", serif !important;
-  margin-bottom: 50px;
-`;
-const SAccordionTitle = styled(Accordion.Title)`
-  &&& {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    font-family: "Song Myung", serif !important;
-  }
-`;
-
-const ProfileContainer = styled.div`
+const TitleWrapper = styled.div`
+  margin-top: 50px;
   display: flex;
-  justify-content: space-around;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
 `;
 
-const Para = styled.p`
-  font-size: 16px;
-  line-height: 30px;
+const Title = styled.div`
+  font-size: 40px;
+  font-weight: 800;
+  margin-bottom: 20px;
+`;
+
+const Explain = styled.div`
+  font-size: 18px;
+  font-weight: 600;
+  margin-bottom: 5px;
+`;
+
+const Accordion = styled.div`
+  width: 100%;
+  padding: 0 40px;
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const SLink = styled(Link)`
+  color: #cd6133;
 `;
 
 const QnA = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [data, setData] = useState({ loading: true, data: [] });
-
-  const handleData = async () => {
-    const res = await axios.get("http://18.221.250.120/api/post");
-    console.log(res);
+  const handleClick = (event) => {
+    if (!event.target.classList.contains("accordian-toggle")) return;
+    let content = document.querySelector(event.target.hash);
+    if (!content) return;
+    event.preventDefault();
+    if (content.classList.contains("active")) {
+      content.classList.remove("active");
+      return;
+    }
+    let accordians = document.querySelectorAll(".accordian-content.active");
+    for (let i = 0; i < accordians.length; i++) {
+      accordians[i].classList.remove("active");
+    }
+    content.classList.add("active");
   };
 
   useEffect(() => {
-    handleData();
+    document.addEventListener("click", handleClick);
+    return () => document.removeEventListener("click", handleClick);
   }, []);
-
-  const handleClick = (event, titleProps) => {
-    const index = titleProps.index;
-    const newIndex = activeIndex === index ? -1 : index;
-    setActiveIndex(newIndex);
-  };
   return (
     <>
       <Header />
       <Container>
-        <SHeader>자주 묻는 질문</SHeader>
-        <SAccordion fluid styled>
-          <SAccordionTitle
-            active={activeIndex === 0}
-            index={0}
-            onClick={handleClick}
-          >
-            <Icon name="dropdown" />
-            <div>홍대병은 어떤 사이트인가요?</div>
-          </SAccordionTitle>
-          <Accordion.Content active={activeIndex === 0}>
-            <Para>
-              홍대병은 SNS연계 의류 홍보 사이트입니다.
+        <TitleWrapper>
+          <Title>홍대병 FAQ</Title>
+          <Explain>자주 묻는 질문을 모았습니다.</Explain>
+          <Explain>
+            추가적으로 궁금하신 부분은 홍대병 오픈카톡으로 문의해주세요.
+          </Explain>
+        </TitleWrapper>
+        <Accordion>
+          <div className="accordian-wrapper">
+            <a href="#content-1" className="accordian-toggle">
+              홍대병은 어떤 사이트인가요?
+            </a>
+            <div className="accordian-content" id="content-1">
+              홍대병은 SNS 연계 의류 홍보 사이트입니다.
               <br />
+              개성을 강조하는 시대에서, 패션은 그 어떤 것보다 자신을 잘
+              나타냅니다.
               <br />
-              홍대병은 사용자 여러분에게 SNS에서 작성된 의류 관련 게시물 정보를
-              보다 쉽고 빠르게 정보 제공을 하기위해
-              <br />
-              그리고 홍보를 하고싶은 사용자 분들에게 홍보를 대행해주기 위해
-              제작됐습니다
-            </Para>
-          </Accordion.Content>
-
-          <SAccordionTitle
-            active={activeIndex === 1}
-            index={1}
-            onClick={handleClick}
-          >
-            <Icon name="dropdown" />
-            <div>회원가입은 어떻게 하나요?</div>
-          </SAccordionTitle>
-          <Accordion.Content active={activeIndex === 1}>
-            <p>알아요.</p>
-          </Accordion.Content>
-
-          <SAccordionTitle
-            active={activeIndex === 2}
-            index={2}
-            onClick={handleClick}
-          >
-            <Icon name="dropdown" />
-            <div>좋아요 누른 옷들을 보고 싶어요!</div>
-          </SAccordionTitle>
-          <Accordion.Content active={activeIndex === 2}>
-            <p>준</p>
-            <p>식</p>
-          </Accordion.Content>
-        </SAccordion>
-        <SHeader>Contact</SHeader>
-        <ProfileContainer>
-          <PersonalInfo
-            name="이찬형"
-            email="lchyung1998@gmail.com"
-            blog="velog.io/@lchyung1998"
-            github="github.com/c01d-br0th3r"
-          />
-          <PersonalInfo
-            name="이찬형"
-            email="lchyung1998@gmail.com"
-            blog="velog.io/@lchyung1998"
-            github="github.com/c01d-br0th3r"
-          />
-          <PersonalInfo
-            name="이찬형"
-            email="lchyung1998@gmail.com"
-            blog="velog.io/@lchyung1998"
-            github="github.com/c01d-br0th3r"
-          />
-          <PersonalInfo
-            name="이찬형"
-            email="lchyung1998@gmail.com"
-            blog="velog.io/@lchyung1998"
-            github="github.com/c01d-br0th3r"
-          />
-        </ProfileContainer>
+              우리는 이러한 트렌드를 따라가 자신만의 개성이 담긴 옷을 다른
+              사람들에게 알려줍니다.
+              <br />전 세계의 쇼핑몰들을 둘러보고 마음에 드는 옷을 구매해보세요!
+            </div>
+          </div>
+          <div className="accordian-wrapper">
+            <a href="#content-2" className="accordian-toggle">
+              질문 2
+            </a>
+            <div className="accordian-content" id="content-2">
+              답변 2
+            </div>
+          </div>
+          <div className="accordian-wrapper">
+            <a href="#content-3" className="accordian-toggle">
+              질문 3
+            </a>
+            <div className="accordian-content" id="content-3">
+              답변 3
+            </div>
+          </div>
+          <div className="accordian-wrapper">
+            <a href="#content-4" className="accordian-toggle">
+              질문 4
+            </a>
+            <div className="accordian-content" id="content-4">
+              답변 4
+            </div>
+          </div>
+          <div className="accordian-wrapper">
+            <a href="#content-5" className="accordian-toggle">
+              질문 5
+            </a>
+            <div className="accordian-content" id="content-5">
+              답변 5
+            </div>
+          </div>
+        </Accordion>
+        <TitleWrapper>
+          <Title>Contact US</Title>
+          <Explain>
+            자세한 내용을 보려면 <SLink to="/contact">여기</SLink>를 클릭하세요.
+          </Explain>
+        </TitleWrapper>
       </Container>
     </>
   );
